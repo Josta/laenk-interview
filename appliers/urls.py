35477c0104bd95eb042411368d852de3671e7cf1
Josua Stabenow <josua.stabenow@gmx.de>
2025-11-10 15:58:57 +0100

    Add faster alternative /appliers/list endpoint

--- appliers/urls.py
+++ appliers/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from appliers.views import ApplierViewSet
+from appliers.views.applier1 import Applier1ViewSet
+from appliers.views.applier2 import Applier2ViewSet
 
 urlpatterns = [
-    path("list/", ApplierViewSet.as_view()),
+    path("list/", Applier1ViewSet.as_view()),
+    path("list2/", Applier2ViewSet.as_view()),
 ]
--- appliers/views/applier2.py
+++ appliers/views/applier2.py
@@ -1,14 +1,18 @@
 from django.views import View
-from django.db.models import Count, Subquery
+from django.db.models import Count, OuterRef, Subquery, IntegerField
 from django.http import JsonResponse
 from appliers.models import Applier, ScreeningQuestion
 
 
-class ApplierViewSet(View):
+class Applier2ViewSet(View):
     def get(self, request, *args, **kwargs):
-        appliers_query = Applier.objects.annotate(
-            question_count=Count("screening_questions")
+        # this avoids an expensive post-join/aggregate having clause by performing a subquery first
+        questions_query = ScreeningQuestion.objects.values("application_id").annotate(
+            question_count=Count("id")
         ).filter(question_count__gt=16)
+        
+        ids = list(map(lambda x: x["application_id"], questions_query))
+        appliers_query = Applier.objects.filter(id__in=ids)
 
         data = []
         for applier in appliers_query:
