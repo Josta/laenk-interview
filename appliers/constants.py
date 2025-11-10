fcb9d3a03bcac17810be95342200a509dea691d9
Josua Stabenow <josua.stabenow@gmx.de>
2025-11-10 18:56:22 +0100

    Add more sensible max distance

--- appliers/constants.py
+++ appliers/constants.py
@@ -16,10 +16,7 @@ QUALIFIED_CHOICES = [
 # Search parameters
 DEFAULT_SEARCH_RADIUS_KM = 20.0
 MIN_RADIUS_KM = 0.1
-MAX_RADIUS_KM = 1000.0
+MAX_RADIUS_KM = 100.0
 
 # Geographic constants
 WGS84_SRID = 4326  # World Geodetic System 1984 coordinate reference system
-
-# Bulk update batch size
-BULK_UPDATE_BATCH_SIZE = 1000
